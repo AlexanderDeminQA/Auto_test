9d68deb86c8505492195fc6395e914d3bfc0500c
AlexanderDeminQA <schurick.don@gmail.com>
2022-12-29 02:27:34 +0300

    browser qut

--- test_google.py
+++ test_google.py
@@ -4,15 +4,21 @@ import pytest
 import time
 
 link = 'https://arenum.io/ru'
+link1 = 'https://arenum.io/ru/scorebeat/tournament/JJbLM4ZYQg-YPpI79Abpkg'
 
-@pytest.fixture
+@pytest.fixture()
 def browser():
-    print("Браузер открылся")
+    print(" Браузер открылся")
     browser = webdriver.Chrome()
-    return browser
+    yield browser
+    print(' Браузер закрыт')
+    browser.quit()
+
 
 
 class TestGoogle():
+    #@pytest.mark.smok
+    @pytest.mark.skip
     def test_redirect_link(self, browser):
         browser.get(link)
         button1 = browser.find_element(By.XPATH, '//div [@class="sb-btn-inner"]')
@@ -21,6 +27,16 @@ class TestGoogle():
         test1 = browser.find_element(By.XPATH, '//h3 [@class="sb-title"]')
         save1 = test1.text
         assert save1 == 'Прошедшие'
+
+class TestArenum():
+   # @pytest.mark.regres
+    #@pytest.mark.win10
+    def test_button_click(self, browser):
+        browser.get(link1)
+        button2 = browser.find_element(By.XPATH, '//div[@class="sb-t-title"]')
+        time.sleep(3)
+        text1 = button2.text
+        assert text1 == 'Flappy Doggo Fast Cup'
 
 
 
